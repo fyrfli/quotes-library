491d44672c4e4df0df6642da7c85b80133e274cd
Camille Frantz <camille.frantz@me.com>
2022-05-28 01:07:20 -0500

    added iteration but not sure it's working

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+'use strict';
 // Full year for footer
 const todayDate = new Date();
 jsyear.innerText = todayDate.getFullYear();
 
 const disp = document.querySelector(".quote");
+let numQuotes;
+let randomNum;
+let intervalID;
+let iteratorID = 0;
 
 fetch("FavouriteQuotes.json")
   .then((response) => {
@@ -10,7 +15,23 @@ fetch("FavouriteQuotes.json")
   })
   .then((data) => {
     numQuotes = Object.entries(data).length;
-    num = Math.floor(Math.random() * numQuotes);
+    randomNum = Math.floor(Math.random() * numQuotes);
     disp.classList.add("anim");
-    disp.innerText = data[num].quote + "\n\n" + data[num].author.replace(/"/g, '');
-  })
+    disp.innerText = data[randomNum].quote + "\n\n" + data[randomNum].author.replace(/"/g, '');
+    disp.classList.remove("anim");
+    
+    intervalID = setInterval(() => {
+      if(iteratorID < numQuotes) {
+        randomNum = Math.floor(Math.random() * numQuotes);
+        disp.classList.add("anim");
+        disp.innerText = data[randomNum].quote + "\n\n" + data[randomNum].author.replace(/"/g, '');
+        disp.classList.remove("anim");
+        iteratorID++;
+      }
+    }, 6000);
+    clearInterval(intervalID);
+    //numQuotes = Object.entries(data).length;
+    //num = Math.floor(Math.random() * numQuotes);
+    //disp.classList.add("anim");
+    //disp.innerText = data[num].quote + "\n\n" + data[num].author.replace(/"/g, '');
+  });
