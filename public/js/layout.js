536e9c242893b97653a3b534dc5235e9f1a29f18
Camille Frantz <camille.frantz@me.com>
2024-11-08 12:01:26 -0600

    search function added in, styled, and working

--- public/js/layout.js
+++ public/js/layout.js
@@ -21,3 +21,36 @@ fetch("https://cdn.fyrfli.link/FavouriteQuotes.json")
       i++;
     }
   });
+
+function liveSearch() {
+  let j;
+  let input = document.getElementById('filter').value.toLowerCase();
+  let item = document.getElementsByClassName('quote');
+  
+  for (j = 0; j < item.length; j++) {
+    if (!item[j].innerText.toLowerCase().includes(input)) {
+      item[j].style.display = "none";
+    }
+    else {
+      item[j].style.display = "inline-block"
+    }
+  }
+}
+
+
+// The working funtion for live search from https://jupiter.fyrfli.net/js-play/jsplay.html
+//   - this is for reference only
+// function search_items() {
+//     let input = document.getElementById('filter').value.toLowerCase();
+//     let x = document.getElementsByClassName('item');
+
+//     for (i = 0; i < x.length; i++) {
+//         if (!x[i].innerHTML.toLowerCase().includes(input)) {
+//             x[i].style.display = "none";
+//         }
+//         else {
+//             x[i].style.display = "list-item";
+//         }
+//     }
+// }
+
